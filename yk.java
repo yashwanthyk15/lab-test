e73de42d2e731f8926a8abc8fc393075cab9bb4c
Yashwanth Kumar S B <147118967+yashwanthyk15@users.noreply.github.com>
2025-12-05 09:46:01 +0530

    Update yk.java

--- yk.java
+++ yk.java
@@ -1,5 +1,5 @@
-public class yk{
-public static void main (String[ args){
-System.out.println("Hello World");
-}
+public class yk {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
